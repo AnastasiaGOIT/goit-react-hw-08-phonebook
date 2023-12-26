461768f58d35caaa6c19f1da6df34f41d5bfdd36
AnastasiaGOIT <132286600+AnastasiaGOIT@users.noreply.github.com>
2023-12-26 11:28:14 +0000

    Deploying to gh-pages from @ AnastasiaGOIT/goit-react-hw-08-phonebook@b2ab9c26a2ea5944e1544a15e7193536fa2951d4 🚀

---
+++ static/js/983.55e94af7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[983],{983:function(e,o,n){n.r(o);var t=n(184);o.default=function(){return(0,t.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=983.55e94af7.chunk.js.map
